2ab17615c4af203a973efccb6331b4d8051334b8
Isaac Dunn <ird28@cam.ac.uk>
2015-12-18 10:30:59 +0000

    Made expressions concrete in Expression interface

--- src/expression.mli
+++ src/expression.mli
@@ -1,11 +1,39 @@
 (* Expressions *)
 (* Isaac Dunn 17/12/2015 *)
 
-(** Expressions with de Bruijn indices *)
-type expr
-
 (** Need some way of referring to locations for stores etc. *)
 type loc = string
+
+type oper =
+     Plus
+   | Minus
+   | Mult
+   | Div
+   | Mod
+   | GT
+   | Equals
+
+(** Expressions with de Bruijn indices *)
+type expr =
+     Integer of int
+   | Boolean of bool
+   | Op of expr * oper * expr
+   | If of expr * expr * expr
+   | Assign of expr * expr
+   | Deref of expr
+   | Ref of expr
+   | Loc of loc
+   | Glo of loc
+   | Skip
+   | Seq of expr * expr
+   | While of expr * expr
+   | Var of int
+   | Fn of Type.type_expr * expr
+   | App of expr * expr
+   | Let of Type.type_expr * expr * expr
+   | Letrec of Type.type_expr * Type.type_expr * expr * expr
+   | Cas of expr * expr * expr
+   | Error of string
 
 (** Gives human-readable representation *)
 val string_of_expr : expr -> string
