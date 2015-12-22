946b7d3e4701a0f2d61cc058792186a8ddb39118
Isaac Dunn <ird28@cam.ac.uk>
2015-12-22 00:48:34 +0000

    Fix simple checker; now appears to work
    First bug fixed. Before, the negation of err_reached was reached,
    which, when coupled with assignment of that reference to the result
    of recursive calls, meant that result returned alernated between true
    and false as the depth in the recursion stack varied. Fixed by using
    no_err_reached instead, requiring no negation.
    
    Second bug fixed. Before, if there were no (global) transitions
    available for a particular thread, the search stopped. However, this did
    not account for the fact that errors are often reached *after* the last
    access of a global object in a thread. Fixed by exploring local thread
    to completion when no transitions available.
    
    Also added more debugging statements, controlled by a module-wide flag.

--- src/checker.ml
+++ src/checker.ml
@@ -3,24 +3,48 @@ module SimpleChecker (Prog : Interfaces.Program) =
     module ProgImp = Prog
     let print_debug = false
 
+    (* True iff error-free *)
     let rec check init_prog t_seq =
         let (tds, g) = List.fold_left ProgImp.apply_transition init_prog t_seq in
-        let err_reached = ref false in
+        let no_err_reached = ref true in
         for i = 0 to Array.length tds - 1 do
             let (e, s) = Array.get tds i in
+            if print_debug then print_endline ("Depth: " ^ (string_of_int (List.length t_seq))
+                ^ " Thread: " ^ (string_of_int i)
+                ^ " Expression: " ^ (ProgImp.ThrImp.ExpImp.string_of_expr e));
             match ProgImp.ThrImp.next_transition (e, s, g) with
-              None -> ()
+              (* No futher transitions: check if this thread reaches a local error *)
+              None ->
+                let rec check_local (ex, st) =
+                    if ProgImp.ThrImp.ExpImp.is_error ex
+                    then (no_err_reached := false; if print_debug then
+                            print_endline ("Local error reached: " ^
+                                (ProgImp.ThrImp.ExpImp.string_of_expr ex)))
+                    else match ProgImp.ThrImp.next_step (ex, st, g) with
+                        None -> if print_debug then print_endline "No local errors" (* No errors *)
+                      | Some t_step -> check_local (t_step.ProgImp.ThrImp.new_expr,
+                                        (match t_step.ProgImp.ThrImp.s_update with
+                                          None -> st
+                                        | Some su -> ProgImp.ThrImp.StoreImp.update st su))
+                in if !no_err_reached then check_local (e, s)
+              (* There is a transition: check if error; explore from the new state *)
             | Some t_tran -> 
-                if ProgImp.ThrImp.ExpImp.is_error t_tran.next_expr
-                then (err_reached := true; if print_debug then print_string "ERROR\n";
+                if ProgImp.ThrImp.ExpImp.is_error t_tran.ProgImp.ThrImp.next_expr
+                (* This is error; mark it as such and print debug *)
+                then (no_err_reached := false; if print_debug then (print_string "ERROR\n";
                  let print_and_apply stat tran =
                   print_string (ProgImp.string_of_program stat);
                   ProgImp.apply_transition stat tran
                  in print_string (ProgImp.string_of_program (
-                    List.fold_left print_and_apply init_prog (t_seq @ [(i, t_tran)]))))
-                else if not !err_reached
-                    then err_reached :=  check init_prog (t_seq @ [(i, t_tran)])
-        done; not !err_reached
+                    List.fold_left print_and_apply init_prog (t_seq @ [(i, t_tran)])))))
+                (* Not an error; explore subsequent transitions *)
+                else if !no_err_reached
+                    then no_err_reached := check init_prog (t_seq @ [(i, t_tran)])
+        done;
+        if print_debug then print_endline ("no_err_reached: "
+            ^ (string_of_bool !no_err_reached)
+            ^ " depth: " ^ (string_of_int (List.length t_seq)));
+        !no_err_reached
   end
 
 module SimplePLChecker : (Interfaces.Checker
