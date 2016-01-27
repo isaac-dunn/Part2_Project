6b15cdc0e99624a95875c66efc5e76223afa3c6f
Isaac Dunn <ird28@cam.ac.uk>
2016-01-27 16:32:47 +0000

    Update PL step tests to use new PL strings

--- src/tests/pl_step_tests.ml
+++ src/tests/pl_step_tests.ml
@@ -9,75 +9,65 @@ open Type
 (* (Expression, Expected result) pairs *)
 let test_cases = [
     (* Integer operations *)
-    (Op (Integer 3, Plus, Integer 7), Integer 10);
-    (Op (Integer 3, Minus, Integer 7), Integer (-4));
-    (Op (Integer 3, Mult, Integer 7), Integer 21);
-    (Op (Integer 3, Div, Integer 7), Integer 0);
-    (Op (Integer 3, Mod, Integer 7), Integer 3);
-    (Op (Integer 3, GT, Integer 7), Boolean false);
-    (Op (Integer 3, Equals, Integer 7), Boolean false);
+    ("3 + 7", "10");
+    ("3 - 7", "-4");
+    ("3 * 7", "21");
+    ("23 / 7", "3");
+    ("23 % 7", "2");
+    ("23 > 7", "true");
+    ("23 = 7", "false");
 
     (* If then else *)
-    (If (Boolean true, Integer 1, Integer 2), Integer 1);
-    (If (Op (Integer 4, Equals, Integer 3), Integer 1, Integer 2), Integer 2);
-    (If (Skip, Integer 3, Integer 3), If (Skip, Integer 3, Integer 3));
+    ("if true then 1 else 2", "1");
+    ("if 4 = 3 then 1 else 2", "2");
 
     (* Sequences *)
-    (Seq (Skip, Integer 4), Integer 4);
+    ("skip; 4", "4");
 
     (* Let *)
-    (convert_from_raw (Let_raw ("x", Int, Integer_raw 9, Var_raw "x")), Integer 9);
-    (convert_from_raw (Let_raw ("w", Int, Integer_raw 10,
-                    Let_raw ("u", Int, Integer_raw 8, Op_raw (Var_raw "w", Minus, Var_raw "u")))),
-        Integer 2);
+    ("let Vx = 9 in Vx", "9");
+    ("let Vw = 10 in let Vu = 8 in Vw - Vu", "2");
+
+    (* Skip *)
+    ("skip", "skip");
 
     (* References *)
-    (Deref (Ref (Integer 9)), Integer 9);
-    (convert_from_raw (Let_raw ("x", Ref Int, Ref_raw (Integer_raw 6),
-                    Seq_raw (Assign_raw (Var_raw "x", Op_raw (Deref_raw (Var_raw "x"), Plus, Integer_raw 7)),
-                         Deref_raw (Var_raw "x")))), Integer 13);
+    ("!(ref 9)", "9");
+    ("(ref 6) := 4", "skip");
+    ("let Vx = ref 6 in Vx := !Vx + 7; !Vx", "13");
 
     (* Functions *)
-    (convert_from_raw (App_raw (Fn_raw ("x", Int, Op_raw (Var_raw "x", Mult, Integer_raw 3)), Integer_raw 9)), Integer 27);
-    (convert_from_raw (Let_raw ("f", Func(Int, Int), Fn_raw ("x", Int, Op_raw (Var_raw "x", Plus, Integer_raw 1)),
-                    App_raw (Var_raw "f", Integer_raw 7))), Integer 8);
-
+    ("(fn Vx => Vx * 3) @ 9", "27");
+    ("let Vf = fn Vx => 1 + Vx in Vf @ 7", "8");
 
     (* While *)
-    (While (Boolean false, Integer 7), Skip);
-    (convert_from_raw (Let_raw ("x", Ref Int, Ref_raw (Integer_raw 34),
-                    Seq_raw (While_raw (Op_raw (Deref_raw (Var_raw "x"), GT, Integer_raw 10),
-                        Assign_raw (Var_raw "x", Op_raw (Deref_raw (Var_raw "x"), Minus, Integer_raw 10))), Deref_raw (Var_raw "x")))),
-        Integer 4); (* subtract 10 from 34 until at most 10 *)
+    ("while false do skip done", "skip");
+    ("let Vx = ref false in while false do Vx := true done; !Vx", "false");
+    ("let Vx = ref 34 in while !Vx > 10 do Vx := !Vx - 10 done; !Vx", "4");
 
     (* Let rec *)
-    (convert_from_raw (Letrec_raw ("f", Int, "x", Int, Var_raw "x", App_raw (Var_raw "f", Integer_raw 5))), Integer 5);
-    (convert_from_raw (Letrec_raw ("f", Int, "x", Int,
-                    If_raw (Op_raw (Var_raw "x", GT, Integer_raw 32),
-                        App_raw (Var_raw "f", Op_raw (Var_raw "x", Div, Integer_raw 2)),
-                        Var_raw "x"),
-                    App_raw (Var_raw "f", Integer_raw 100))),
-        Integer 25);
-    (convert_from_raw (Letrec_raw ("f", Int, "x", Int,
-                    If_raw (Op_raw (Var_raw "x", Equals, Integer_raw 0), Integer_raw 1,
-                            Op_raw (Var_raw "x", Mult, App_raw (Var_raw "f", Op_raw (Var_raw "x", Minus, Integer_raw 1)))),
-                    App_raw (Var_raw "f", Integer_raw 4))),
-        Integer 24)
+    ("let rec Vf = fn Vx => Vx - 1 in Vf @ 5", "4");
+    ("let rec Vg = fn Vy => if 32 > Vy then Vy else Vg @ (Vy/2) in Vg @ 100", "25");
+    ("let rec Vfact = fn Vz => if Vz = 0 then 1 else Vz * (Vfact @ (Vz-1)) in Vfact @ 5", "120");
+
+    (* Error *)
+    ("error(oh dear)", "error(oh dear)");
 ]
 
-let run_test (e, expected) =
+let run_test (e_str, expected_str) =
+    let e = Pl_parser.expr_of_string e_str in
+    let expected = Pl_parser.expr_of_string expected_str in
     let unpack su s = match su with None -> s
                                   | Some x -> Thr.StoreImp.update s x in
     let rec eval (e, s, g) = match Thr.next_step (e, s, g) with
         None -> e
-      | Some nsx -> eval (nsx.new_expr, unpack nsx.s_update s, unpack nsx.g_update g)
+      | Some nsx -> eval (nsx.Thr.new_expr, unpack nsx.Thr.s_update s, unpack nsx.Thr.g_update g)
     in if eval (e, Thr.StoreImp.empty, Thr.StoreImp.empty) = expected
-        then true (* success *)
+        then (print_endline ("Success: " ^ e_str ^ " --> " ^ expected_str); true) (* success *)
         else (print_string "Expected: ";
-             print_string (Thr.ExpImp.string_of_expr expected);
-             print_newline ();
+             print_endline (Thr.ExpImp.string_of_expr expected);
              print_string "Actual: ";
-             print_string (Thr.ExpImp.string_of_expr (eval (e, Thr.StoreImp.empty, Thr.StoreImp.empty)));
+             print_endline (Thr.ExpImp.string_of_expr (eval (e, Thr.StoreImp.empty, Thr.StoreImp.empty)));
              false) (* fail *)
 
 let all_tests_passed =
