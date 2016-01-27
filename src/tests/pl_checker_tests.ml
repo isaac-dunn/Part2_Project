8785bbb14d17f53e30f83de037206a7b38942914
Isaac Dunn <ird28@cam.ac.uk>
2016-01-27 17:52:24 +0000

    Use function rather than massive string for arrays

--- src/tests/pl_checker_tests.ml
+++ src/tests/pl_checker_tests.ml
@@ -14,50 +14,11 @@ module PLCorrectnessTest (Chk :
         let rec array_store n = if n = 0 then [("0", Integer 0)]
             else  (string_of_int n,
                     Integer 0)::(array_store (n-1)) in
-        let int_to_loc_expr_str = "fn x =>
-            if x=0 then G0 else if x=1 then G1 else if x=2 then G2 else
-            if x=3 then G3 else if x=4 then G4 else if x=5 then G5 else
-            if x=6 then G6 else if x=7 then G7 else if x=8 then G8 else
-            if x=9 then G9 else if x=10 then G10 else if x=11 then G11 else
-            if x=12 then G12 else if x=13 then G13 else if x=14 then G14 else
-            if x=15 then G15 else if x=16 then G16 else if x=17 then G17 else
-            if x=18 then G18 else if x=19 then G19 else if x=20 then G20 else
-            if x=21 then G21 else if x=22 then G22 else if x=23 then G23 else
-            if x=24 then G24 else if x=25 then G25 else if x=26 then G26 else
-            if x=27 then G27 else if x=28 then G28 else if x=29 then G29 else
-            if x=30 then G30 else if x=31 then G31 else if x=32 then G32 else
-            if x=33 then G33 else if x=34 then G34 else if x=35 then G35 else
-            if x=36 then G36 else if x=37 then G37 else if x=38 then G38 else
-            if x=39 then G39 else if x=40 then G40 else if x=41 then G41 else
-            if x=42 then G42 else if x=43 then G43 else if x=44 then G44 else
-            if x=45 then G45 else if x=46 then G46 else if x=47 then G47 else
-            if x=48 then G48 else if x=49 then G49 else if x=50 then G50 else
-            if x=51 then G51 else if x=52 then G52 else if x=53 then G53 else
-            if x=54 then G54 else if x=55 then G55 else if x=56 then G56 else
-            if x=57 then G57 else if x=58 then G58 else if x=59 then G59 else
-            if x=60 then G60 else if x=61 then G61 else if x=62 then G62 else
-            if x=63 then G63 else if x=64 then G64 else if x=65 then G65 else
-            if x=66 then G66 else if x=67 then G67 else if x=68 then G68 else
-            if x=69 then G69 else if x=70 then G70 else if x=71 then G71 else
-            if x=72 then G72 else if x=73 then G73 else if x=74 then G74 else
-            if x=75 then G75 else if x=76 then G76 else if x=77 then G77 else
-            if x=78 then G78 else if x=79 then G79 else if x=80 then G80 else
-            if x=81 then G81 else if x=82 then G82 else if x=83 then G83 else
-            if x=84 then G84 else if x=85 then G85 else if x=86 then G86 else
-            if x=87 then G87 else if x=88 then G88 else if x=89 then G89 else
-            if x=90 then G90 else if x=91 then G91 else if x=92 then G92 else
-            if x=93 then G93 else if x=94 then G94 else if x=95 then G95 else
-            if x=96 then G96 else if x=97 then G97 else if x=98 then G98 else
-            if x=99 then G99 else if x=100 then G100 else if x=101 then G101 else
-            if x=102 then G102 else if x=103 then G103 else if x=104 then G104 else
-            if x=105 then G105 else if x=106 then G106 else if x=107 then G107 else
-            if x=108 then G108 else if x=109 then G109 else if x=110 then G110 else
-            if x=111 then G111 else if x=112 then G112 else if x=113 then G113 else
-            if x=114 then G114 else if x=115 then G115 else if x=116 then G116 else
-            if x=117 then G117 else if x=118 then G118 else if x=119 then G119 else
-            if x=120 then G120 else if x=121 then G121 else if x=122 then G122 else
-            if x=123 then G123 else if x=124 then G124 else if x=125 then G125 else
-            if x=126 then G126 else if x=127 then G127 else error(out of bounds)" in
+        let int_to_loc_str n =
+            let itls_aux m = if m > n then "error(index out of bounds)"
+                    else "if x=" ^ (string_of_int m) ^ " then G" ^
+                            (string_of_int m) ^ "else " ^ (itls_aux (m+1))
+            in "fn x => " ^ (itls_aux 0) in
 
     [
         (* Test 0 *)
@@ -165,7 +126,7 @@ module PLCorrectnessTest (Chk :
                 done
             done";
 
-        |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+        |], ("table", Pl_parser.expr_of_string int_to_loc_str 128)::
                 (array_store 128), false);
 
         (* Test 14 *)
@@ -190,7 +151,7 @@ module PLCorrectnessTest (Chk :
                 else error(array element already reached)
              done";
 
-         |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+         |], ("table", Pl_parser.expr_of_string int_to_loc_str 128)::
                 (array_store 128), false);
 
         (* Test 15 *)
@@ -215,7 +176,7 @@ module PLCorrectnessTest (Chk :
                 else i := !i + 1
              done";
 
-         |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+         |], ("table", Pl_parser.expr_of_string int_to_loc_str 128)::
                 (array_store 128), true);
 
         (* Test 16 *)
@@ -242,7 +203,7 @@ module PLCorrectnessTest (Chk :
                      else i := !i + 1
             done)";
 
-         |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+         |], ("table", Pl_parser.expr_of_string int_to_loc_str 8)::
                (("ready", Boolean false)::(array_store 8)), false);
 
         (* Test 17 *)
@@ -279,7 +240,7 @@ module PLCorrectnessTest (Chk :
                 else ctr := !ctr + 1
              done";
 
-         |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+         |], ("table", Pl_parser.expr_of_string int_to_loc_str 8)::
                (array_store 8), false);
 
         (* Test 18 *)
@@ -310,7 +271,7 @@ module PLCorrectnessTest (Chk :
                      else i := !i + 1
             done";
 
-         |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+         |], ("table", Pl_parser.expr_of_string int_to_loc_str 8)::
              ("size", Integer 4)::("mod", Integer 4)::(array_store 8), false);
 
         (* Beer in fridge example from Part IB Conc. Systems *)
@@ -371,7 +332,7 @@ module PLCorrectnessTest (Chk :
                      else pi := !pi + 1
             done";
 
-         |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+         |], ("table", Pl_parser.expr_of_string int_to_loc_str 100)::
              ("size", Integer 3)::(array_store 100), false);
 
         (* Test 22 *)
@@ -417,7 +378,7 @@ module PLCorrectnessTest (Chk :
                   else i := !i + 2
              done";
 
-         |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+         |], ("table", Pl_parser.expr_of_string int_to_loc_str 8)::
              ("size", Integer 2)::(array_store 8), false);
 
         (* Test 23 *)
@@ -448,7 +409,7 @@ module PLCorrectnessTest (Chk :
                 else error(should not be set)
              done";
 
-         |], ("table", Pl_parser.expr_of_string int_to_loc_expr_str)::
+         |], ("table", Pl_parser.expr_of_string int_to_loc_str 100)::
              ("increment", Integer 3)::("size", Integer 4)::
              (array_store 100), false);
     ]
