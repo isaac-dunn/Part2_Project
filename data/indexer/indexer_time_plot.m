207b7debb54b6f9a0e5e327da45c24ded602dbbd
Isaac Dunn <ird28@cam.ac.uk>
2016-05-05 19:49:51 +0100

    Update other plotters and figs (new ss)

--- data/indexer/indexer_time_plot.m
+++ data/indexer/indexer_time_plot.m
@@ -1,14 +1,16 @@
 %% importing data
 transitions = dlmread('indexer_time_data.csv', ',', 1, 1);
+ss = dlmread('ss.csv', ',', 0, 0);
 % convert 0 values to NaNs, as these don't plot
 transitions(transitions==0)=nan;
 threads = [1:17];
+ssthreads = [1:3];
 
 %% plotting lines
 
 semilogy(threads, transitions(:, 1), 'ko-')
 hold on
-semilogy(threads, transitions(:, 2), 'kx-')
+semilogy(ssthreads, ss(:, 2), 'kx-')
 semilogy(threads, transitions(:, 3), 'ks-')
 semilogy(threads, transitions(:, 4), 'bo-')
 semilogy(threads, transitions(:, 5), 'bx-')
--- data/prod_cons/prod_cons_time_plot.m
+++ data/prod_cons/prod_cons_time_plot.m
@@ -1,14 +1,16 @@
 %% importing data
 transitions = dlmread('prod_cons_time_data.csv', ',', 1, 1);
+ss = dlmread('ss.csv', ',', 0, 0);
 % convert 0 values to NaNs, as these don't plot
 transitions(transitions==0)=nan;
 threads = [1:20];
+ssthreads= [1:6];
 
 %% plotting lines
 
 semilogy(threads, transitions(:, 1), 'ko-')
 hold on
-semilogy(threads, transitions(:, 2), 'kx-')
+semilogy(ssthreads, ss(:, 2), 'kx-')
 semilogy(threads, transitions(:, 3), 'ks-')
 semilogy(threads, transitions(:, 4), 'bo-')
 semilogy(threads, transitions(:, 5), 'bx-')
