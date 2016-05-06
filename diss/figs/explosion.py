dbd611a0f0bcb865191a8ea4c55d0f6d0fca5a70
Isaac Dunn <ird28@cam.ac.uk>
2016-05-06 15:37:08 +0100

    Working explosion gv generator

--- diss/figs/explosion.py
+++ diss/figs/explosion.py
@@ -4,11 +4,13 @@ with open('explosion'+sys.argv[1]+'.gv', 'w') as f:
     def output_edges(sofar, threads):
         for p in range(threads):
             if (not (p in sofar)):
-                sf = ''.join(str(x) for x in sofar)
-                edge = sf + ' -> ' + sf + str(p)
-                if sofar != []:
-                    f.write(edge)
-                    f.write('\n')
+                sf = ''.join(['root'] + [str(x) for x in sofar])
+                edge = '  ' + sf + ' -> ' + sf + str(p) + '[label="' + str(p) + '"];\n'
+                f.write(edge)
                 output_edges(sofar + [p], threads)
 
-    output_edges([], 3)
+    prelim = 'digraph Explosion {\n  node [shape=circle, fixedsize=true, label=""];\n'
+    postlim = '}'
+    f.write(prelim)
+    output_edges([], int(sys.argv[1]))
+    f.write(postlim)
